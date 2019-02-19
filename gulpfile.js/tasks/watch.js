4f9c3961fa3b7cc7377cb4712c49886166505110
Daniel Peters <r500dwp@outlook.com>
2019-02-19 22:22:54 +0000

    watch

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -2,23 +2,23 @@ const gulp = require("gulp");
 const browser = require("browser-sync");
 const projectPath = require("../lib/projectPath");
 
-stylesheets_paths = {
-  src: [
-    projectPath(PATH_CONFIG.BASE, PATH_CONFIG.stylesheets.src, "**/*.scss")
-  ],
-  dest: projectPath(PATH_CONFIG.buildDest)
-};
-
-javascripts_paths = {
-  src: [projectPath(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src, "**/*.js")],
-  dest: projectPath(PATH_CONFIG.buildDest)
-};
-
 gulp.task("watch", function() {
-  gulp.watch(
-    ["./lab/**/*", "./html/**/*"],
-    ["html", "lab_html", browser.reload]
-  );
+  html_paths = {
+    src: [
+      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.html.src, "**/*.html"),
+      projectPath(PATH_CONFIG.lab, "**/*.html")
+    ]
+  };
+
+  stylesheets_paths = {
+    src: projectPath(PATH_CONFIG.BASE, PATH_CONFIG.stylesheets.src, "**/*.scss")
+  };
+
+  javascripts_paths = {
+    src: projectPath(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src, "**/*.js")
+  };
+
+  gulp.watch(html_paths, ["html", "lab_html", browser.reload]);
   gulp.watch(stylesheets_paths.src, ["stylesheets", browser.reload]);
   gulp.watch(javascripts_paths.src, ["javascripts", browser.reload]);
 });
