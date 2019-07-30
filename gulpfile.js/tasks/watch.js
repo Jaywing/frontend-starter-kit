7d087e08aa498f93e35ab001857427940a49cf41
Dapen <spartdan@live.co.uk>
2019-07-30 13:18:52 +0100

    fix watch error

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -22,5 +22,5 @@ gulp.task("watch", function() {
 
   gulp.watch(paths.htmlSrc, ["html", browser.reload]);
   gulp.watch(paths.stylesheetsSrc, ["stylesheets", browser.reload]);
-  gulp.watch(paths.javascriptsSrc, ["javascripts", browser.reload]);
+  gulp.watch(paths.javascriptsSrc, ["webpack", browser.reload]);
 });
