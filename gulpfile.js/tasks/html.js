cd50531dee4374ed812e4feaa3646e396fb4c911
Dapen <spartdan@live.co.uk>
2019-08-20 10:39:33 +0100

    Update lab and site

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -9,6 +9,16 @@ const fs = require("fs");
 gulp.task("html", function() {
   paths = {
     src: [
+      `./node_modules/giza-framework/html/**/*.html`,
+      "!" +
+        projectPath(
+          `./node_modules/giza-framework/html/**/{layouts,macros,data}/**`
+        ),
+        `./node_modules/jaywing-frontend-component-library/html/**/*.html`,
+      "!" +
+        projectPath(
+          `./node_modules/jaywing-frontend-component-library/html/**/{layouts,macros,data}/**`
+        ),
       projectPath(PATH_CONFIG.BASE, PATH_CONFIG.html.src, "**/*.html"),
       "!" +
         projectPath(
