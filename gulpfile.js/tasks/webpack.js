76e88dad9f854660862e3057a0760a39fc218435
Dapen <spartdan@live.co.uk>
2019-11-11 17:08:35 +0000

    Production js added

--- gulpfile.js/tasks/webpack.js
+++ gulpfile.js/tasks/webpack.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const gulpif = require("gulp-if");
 const path = require("path");
 const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
@@ -37,6 +38,39 @@ const webpackConfig = {
   }
 };
 
+const webpackConfig_production = {
+  context: path.resolve(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src),
+  entry: {
+    app: "./app.js"
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src),
+    filename: "app.js",
+    publicPath: "/assets/javascripts/"
+  },
+  plugins: [],
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [
+      path.resolve(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src),
+      path.resolve(PATH_CONFIG.BASE, "node_modules")
+    ]
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        // test: /\.js$/,
+        exclude: path.resolve(PATH_CONFIG.BASE, "node_modules"),
+        query: {
+          presets: [["es2015", { modules: false }], "stage-1", "react-app"]
+        }
+      }
+    ]
+  }
+};
+
 gulp.task("webpack", function() {
   paths = {
     src: [
@@ -47,6 +81,7 @@ gulp.task("webpack", function() {
 
   return gulp
     .src(paths.src)
-    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(gulpif(!production, webpackStream(webpackConfig, webpack)))
+    .pipe(gulpif(production, webpackStream(webpackConfig_production, webpack)))
     .pipe(gulp.dest(paths.dest));
 });
